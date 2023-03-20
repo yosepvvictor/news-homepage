d3598ad189ee559435dbaa2f0c477f55f66fb560
yosepvvictor <victoryosep221@gmail.com>
2023-03-20 19:58:14 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        softOrange: 'hsl(35, 77%, 62%)',
+        softRed: 'hsl(5, 85%, 63%)',
+        offWhite: 'hsl(36, 100%, 99%)',
+        grayishBlue: 'hsl(233, 8%, 79%)',
+        darkGrayishBlue: 'hsl(236, 13%, 42%)',
+        veryDarkBlue: 'hsl(240, 100%, 5%)',
+        overlay: 'rgba(0,0,0,0.6)'
+      }
+    },
+  },
+  plugins: [],
+}
